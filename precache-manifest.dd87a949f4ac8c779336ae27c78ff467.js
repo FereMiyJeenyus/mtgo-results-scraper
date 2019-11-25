afc2a3dee42d430706e0ed0273b0f066db537224
FereMiyJeenyus <feremiyjeenyus@gmail.com>
2019-11-24 20:48:02 -0600

    Updates

---
+++ precache-manifest.dd87a949f4ac8c779336ae27c78ff467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92c0386972d6e4638a65",
+    "url": "/mtgo-results-scraper/static/js/runtime~main.7ba72ccb.js"
+  },
+  {
+    "revision": "8a6c479127a9f381b8ea",
+    "url": "/mtgo-results-scraper/static/js/main.61e5eae3.chunk.js"
+  },
+  {
+    "revision": "c97fff2eee5322f7aa03",
+    "url": "/mtgo-results-scraper/static/js/2.00d1d199.chunk.js"
+  },
+  {
+    "revision": "8a6c479127a9f381b8ea",
+    "url": "/mtgo-results-scraper/static/css/main.21eac5c5.chunk.css"
+  },
+  {
+    "revision": "529958213dae17b16c6d19b253f68cda",
+    "url": "/mtgo-results-scraper/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mtgo-results-scraper/precache-manifest.db5f372cf295704510e0ee2adc238ce2.js"
+  "/mtgo-results-scraper/precache-manifest.dd87a949f4ac8c779336ae27c78ff467.js"
 );
 
 workbox.clientsClaim();
