09c7151abb5f53021a3d056a00a7ade32ab5c6fe
FereMiyJeenyus <feremiyjeenyus@gmail.com>
2019-11-24 21:10:49 -0600

    Updates

---
+++ precache-manifest.e9908e89d52dd733a331e006b8c540d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92c0386972d6e4638a65",
+    "url": "/mtgo-results-scraper/static/js/runtime~main.7ba72ccb.js"
+  },
+  {
+    "revision": "4734f042c6a30fd9ff4e",
+    "url": "/mtgo-results-scraper/static/js/main.3b2f6c02.chunk.js"
+  },
+  {
+    "revision": "87e3bdd72ca35e8ef806",
+    "url": "/mtgo-results-scraper/static/js/2.8ba3bbc7.chunk.js"
+  },
+  {
+    "revision": "4734f042c6a30fd9ff4e",
+    "url": "/mtgo-results-scraper/static/css/main.21eac5c5.chunk.css"
+  },
+  {
+    "revision": "f11817b4c381d18b0d65dc6459b374a6",
+    "url": "/mtgo-results-scraper/index.html"
+  }
+];
